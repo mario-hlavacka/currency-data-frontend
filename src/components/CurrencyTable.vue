b89c6e657e6f5e8693775622711ed2631252dee2
Mario Hlavacka <mario.hlavacka@gmail.com>
2025-05-06 13:05:53 +0200

    feat: create CurrencyTable component

---
+++ src/components/CurrencyTable.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Currency Table</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th v-for="header in ['name', 'icon', 'current_price', 'percent_change_24h', 'market_cap']">{{ header }}</th>
+            </tr>
+        </thead>
+        <tbody v-for="currency in [1,2,3,4,5,6]">
+            <tr>
+                <td>{{ currency }}</td>
+                <td>{{ currency }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,11 +5,13 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import CurrencyTable from './CurrencyTable.vue'
 
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
 
 <template>
+  <CurrencyTable />
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
